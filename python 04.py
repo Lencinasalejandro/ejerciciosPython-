08e01e4df509590cccc3f4e5942599648af8bd2a
Lencinasalejandro <lencinasalejandro@yahoo.com.ar>
2022-11-30 10:44:13 -0300

    guia 04 ok

--- python 04.py
+++ python 04.py
@@ -1,32 +1,64 @@
 class Vehiculo():
-    color=input("Ingrese el color del vehiculo: ")
-    ruedas=int(input("Ingrese la cantidad de ruedas: "))
+    def __init__(self):
+        self.color=input("Ingrese el color del vehiculo: ")
+        self.ruedas=int(input("Ingrese la cantidad de ruedas: "))
     
+    def __str__(self):
+        return (f'\nEl vehiculo es {self.color} y tiene {self.ruedas} ruedas.\n')
+        
 class Coche(Vehiculo):
-    velocidad=int(input("Ingrese la velocidad del vehiculo: "))
-    cilindrada=int(input("Ingrese la cilindrada del vehiculo: "))
+    def __init__(self):
+        super().__init__()
+        self.velocidad=int(input("Ingrese la velocidad del vehiculo: "))
+        self.cilindrada=int(input("Ingrese la cilindrada del vehiculo: "))
     
-class Camioneta(Coche):
-    carga=int(input("Ingrese la carga maxima del vehiculo: "))
+    def __str__(self):
+        return super().__str__() + (f'Su velocidad es {self.velocidad} y su cilindrada es {self.cilindrada}.\n')   
+
+class Camioneta(Vehiculo):
+    def __init__(self):
+        super().__init__()
+        self.carga=int(input("Ingrese la carga maxima del vehiculo: "))
+    
+    def __str__(self):
+        return super().__str__() + (f'Su carga maxima es {self.carga}.\n')   
+
     
 class Bicicleta(Vehiculo):
-    while True:
-        tipo=input("Ingrese si es Urbana o Deportiva: ")
-        if (tipo!="Urbana" and tipo !="Deportiva"):
-            print("El tipo ingresado fue incorrecto.")
-        else:
-            break
-
-class Moto(Bicicleta):
-    velocidad=int(input("Ingrese la velocidad del vehiculo: "))
-    cilinddrada=int(input("Ingrese la cilindrada del vehiculo: "))
+    def __init__(self):
+        super().__init__()
+        while True:
+            self.tipo=input("Ingrese si es Urbana o Deportiva: ")
+            if (self.tipo!="Urbana" and self.tipo !="Deportiva"):
+                print("El tipo ingresado fue incorrecto.")
+            else:
+                break
     
-listado=[]
+    def __str__(self):
+        return super().__str__() + (f'La bicicleta es {self.tipo}.\n')   
 
-listado.append(Coche())
-listado.append(Camioneta())
-listado.append(Bicicleta())
-listado.append(Moto())
 
-print(listado)
+class Moto(Vehiculo):
+    def __init__(self):
+        super().__init__()
+        self.velocidad=int(input("Ingrese la velocidad del vehiculo: "))
+        self. cilindrada=int(input("Ingrese la cilindrada del vehiculo: "))
+    
+    def __str__(self):
+        return super().__str__() + (f'La velocidad de la moto es {self.velocidad} y su cilindrada es {self.cilindrada}.\n')   
+
+
+if __name__=='__main__':
+    
+    listado=[]
+
+    listado.append(Coche())
+    print (listado[0])
+    listado.append(Camioneta())
+    listado.append(Bicicleta())
+    print(listado[2])
+    listado.append(Moto())
+    
+    for elemento in listado:
+        print(elemento)
 
